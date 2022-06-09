997b66e77f611ef47d7d5ff6aac0aa4bd51eb460
David Jammer <david.jammer@cea-wismar.de>
2022-06-09 13:46:43 +0200

    Description for the test bench added

--- Testbench/test_models.m
+++ Testbench/test_models.m
@@ -2,6 +2,11 @@ function tests = test_models
 	tests = functiontests(localfunctions);
 end
 
+%Run the models in the Examplex directory and compare the results 
+%with the saved results.
+%
+%run with: run(test_models)
+
 function test_model1(testCase)
 
 	act_out = model1(10);
